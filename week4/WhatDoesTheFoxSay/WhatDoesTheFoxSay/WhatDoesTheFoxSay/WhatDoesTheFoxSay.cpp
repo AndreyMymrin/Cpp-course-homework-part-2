cc6eabc99079b9564cffa431c2ce5ebaca1476e2
AndreyMymrin <mimrin2018@yandex.ru>
2022-09-27 21:38:35 +0300

    animal hometask

---
+++ week4/WhatDoesTheFoxSay/WhatDoesTheFoxSay/WhatDoesTheFoxSay/WhatDoesTheFoxSay.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+enum class animals {
+    dog,
+    cat,
+    cow,
+    bird,
+    fish,
+    man
+};
+
+void WhatDoesTheFoxSay(animals WhyAreYou) {
+    switch (WhyAreYou){
+        case animals::dog: {std::cout << "gavgav" << std::endl; break;}
+        case animals::cat: {std::cout << "maymay" << std::endl; break;}
+        case animals::cow: {std::cout << "mymymy" << std::endl; break;}
+        case animals::bird:{std::cout << "aaaa!!" << std::endl; break;}
+        case animals::fish:{std::cout << "......" << std::endl; break;}
+        case animals::man: {std::cout << "Hallo Word!" << std::endl; break;}
+
+        default: {std::cout << "New animal? Eate it!" << std::endl; break;}
+    }
+}
+
+int main()
+{
+    WhatDoesTheFoxSay(animals::cow);
+    return(0);
+}
