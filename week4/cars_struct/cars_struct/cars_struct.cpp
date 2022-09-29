8d8d6acf3de942f7862e47d04f8c5a0f9db88fff
AndreyMymrin <mimrin2018@yandex.ru>
2022-09-29 20:52:50 +0300

    Update cars_struct.cpp

--- week4/cars_struct/cars_struct/cars_struct.cpp
+++ week4/cars_struct/cars_struct/cars_struct.cpp
@@ -1,20 +1,51 @@
-﻿// cars_struct.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include<string.h>
+struct cars {
+    int max_speed;
+    int number_of_sits;
+    int cost;
+    bool elecrocar;
+    bool is_it_can_fly;
+    std::string name_of_company;
+    std::string colour;
 
-#include <iostream>
+    std::string is_for_family(){
+
+        if (max_speed < 90 && cost < 1000000 && number_of_sits >= 4) {
+            return "Yes";
+        }
+        else {
+            return "No";
+        }
+    };
+
+    int how_cool_is_the_car() {
+        int points = 0;
+        if (name_of_company == "LADA") { points += 50; }
+        else if (elecrocar == 1) { points += 25; }
+
+        if (colour == "red") { max_speed += 50;}
+        points += max_speed % 10;
+        points -= number_of_sits;
+        points += cost / 1000000;
+        if (is_it_can_fly == 1) { points += 100;}
+        return points;
+    };
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cars car_1 = {2000, 4, 20000, 0, 0, "LADA", "red"};
+    cars car_2 = { 100, 2, 50000000, 1, 0, "Tesla", "black" };
+    cars car_3 = { 50, 6, 10000,0,1,"MIPT", "white" };
+    
+    std::cout << "Points of car_1: " << car_1.how_cool_is_the_car() << std::endl;
+    std::cout << "Points of car_2: " << car_2.how_cool_is_the_car() << std::endl;
+    std::cout << "Points of car_3: " << car_3.how_cool_is_the_car() << std::endl;
+
+    std::cout << "Is car_1 for family: " << car_1.is_for_family() << std::endl;
+    std::cout << "Is car_2 for family: " << car_2.is_for_family() << std::endl;
+    std::cout << "Is car_3 for family: " << car_3.is_for_family() << std::endl;
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
