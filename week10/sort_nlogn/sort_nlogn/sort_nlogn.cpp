11b5d443b989d495aabab44c3315eacea584fa56
AndreyMymrin <113207976+AndreyMymrin@users.noreply.github.com>
2022-11-26 15:24:58 +0300

    nlogn sort

--- week10/sort_nlogn/sort_nlogn/sort_nlogn.cpp
+++ week10/sort_nlogn/sort_nlogn/sort_nlogn.cpp
@@ -3,7 +3,48 @@
 #include <math.h>
 #include <time.h>
 template<class Type>
-void sort_qsort(Type* items, int left, int right);
+bool compare_bolshe(Type left_item, Type right_item) {
+    if (left_item > right_item) return 1;
+    else return 0;
+    //(left_item.size() > right_item.size())
+    // 
+    // (abs(left_item) > abs(right_item))
+}
+
+template<class Type>
+void sort_qsort(Type* items, int left, int right, bool(*compare)(Type, Type) = compare)
+{
+    int i, j;
+    Type x, y;
+    i = left;
+    j = right;
+    x = items[(left + right) / 2];
+    do
+    {
+        while (compare(x, items[i]) && (i < right)) { i++; }
+        while (compare(items[j], x) && (j > left)) { j--; }
+
+        if (i <= j)
+        {
+            y = items[i];
+            items[i] = items[j];
+            items[j] = y;
+            i++;
+            j--;
+        }
+    } while (i <= j);
+
+    if (left < j) sort_qsort<Type>(items, left, j, compare);
+    if (i < right) sort_qsort<Type>(items, i, right, compare);
+}
+
+
+
+
+template <class Type, int N>
+void qsort(Type(&arr)[N]) {
+    sort_qsort<Type>(arr, 0, N-1, &compare_bolshe);
+}
 
 template<class Type>
 void print(Type* items, int n) {
@@ -15,20 +56,12 @@ void print(Type* items, int n) {
     std::cout << std::endl;
 }
 
-template<class Type>
-bool compare_bolshe(Type left_item, Type right_item) {
-	if (left_item.size() > right_item.size()) return 1;
-	else return 0;
-
-	// (left_item > right_item)
-	// (abs(left_item) > abs(right_item))
-}
 
 int main()
 {
     srand(time(NULL));
 
-    const int n = 5;
+    const int n = 10;
     std::string a[n];
 
     for (int i = 0; i < n; i++)
@@ -38,39 +71,9 @@ int main()
     }
     print(&a[0], n);
 
-    sort_qsort<std::string>(&a[0], 0, n - 1);
+    qsort<std::string>(a);
 
     print(&a[0], n);
 
     return(0);
-}
-
-
-template<class Type>
-void sort_qsort(Type* items, int left, int right)
-{
-    int i, j;
-    Type x, y;
-    i = left;
-    j = right;
-    x = items[(left + right) / 2];
-    do
-    {
-        while (compare_bolshe<Type>(x, items[i]) && (i < right)) { i++; }
-        while (compare_bolshe<Type>(items[j], x) && (j > left)) { j--; }
-
-        if (i <= j)
-        {
-            y = items[i];
-            items[i] = items[j];
-            items[j] = y;
-            i++;
-            j--;
-        }
-
-    } while (i <= j);
-
-
-    if (left < j) sort_qsort<Type>(items, left, j);
-    if (i < right) sort_qsort<Type>(items, i, right);
 }
