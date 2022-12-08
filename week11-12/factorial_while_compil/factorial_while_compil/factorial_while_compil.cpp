030b357a2590a4d9bf5d90bdeb0962b5c72d556b
AndreyMymrin <113207976+AndreyMymrin@users.noreply.github.com>
2022-12-08 14:08:07 +0300

    factorial

---
+++ week11-12/factorial_while_compil/factorial_while_compil/factorial_while_compil.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+template<int n>
+class fact {
+public:
+    static const int number = fact<n - 1>::number * n;
+};
+
+template<>
+class fact<0> {
+public:
+    static const int number = 1;
+};
+
+int main() {
+    int n = 5;
+    std::cout << n << "! = " << fact<5>::number;
+    
+}
